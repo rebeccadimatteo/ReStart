6b7b5f9b10cc4f65d8dcae69ab15c68286fa43ab
Sere6899 <serena.passiflora@yahoo.it>
2024-01-04 20:03:46 +0100

    "add di candidatura_DAO add di candidatura_DAO_impl, add di candidatura_DTO"

---
+++ ReStart/lib/model/dao/candidatura_lavoro/candidatura_DAO.dart
@@ -0,0 +1,12 @@
+import '../../entity/candidatura_DTO.dart';
+
+/// Questa classe rappresenta l'implementazione del dao della Candidatura
+abstract class CandidaturaDAO {
+  Future<bool> add(CandidaturaDTO ca);
+  Future<bool> existByIdUtente(int id_utente);
+  Future<bool> existByIdLavoro(int id_lavoro);
+  Future<List<CandidaturaDTO>> findAll();
+  Future<CandidaturaDTO?> findByIdUtente(int id_utente);
+  Future<CandidaturaDTO?> findByIdLavoro(int id_lavoro);
+  Future<bool> removeById(int id_lavoro, int id_utente);
+}
