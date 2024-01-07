f2d964ff0437af20b23b6051ad7670757cd7d9db
FCorcione02 <f.corcione5@studenti.unisa.it>
2024-01-08 00:50:37 +0100

    update corso_di_formazione_DAO/DTO update server.dart

--- ReStart/lib/application/bin/server.dart
+++ ReStart/lib/application/bin/server.dart
@@ -1,43 +1,59 @@
+import 'dart:io';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
-
+import 'package:shelf_router/shelf_router.dart' as shelf_router;
 import '../gestioneReintegrazione/controller/reintegrazione_controller.dart';
 
 void main() {
-  final handler = const Pipeline()
-      .addMiddleware(logRequests()) // middleware per registrare le richieste
-      .addHandler((Request request) async {
-    // handler principale per gestire le richieste
+  final app = shelf_router.Router();
+  // Aggiungi i controller al router
+  // app.mount('/autenticazione', AutenticazioneController().router);
+  // app.mount('/candidaturaLavoro', CandidaturaLavoroController().router);
+  // app.mount('/gestioneEvento', GestioneEventoController().router);
+  // app.mount('/gestioneLavoro', GestioneLavoroController().router);
+  app.mount('/gestioneReintegrazione', ReintegrazioneController().router);
+  //app.mount('/lavoroAdatto', LavoroAdattoController().router);
+  //app.mount('/registrazione', RegistrazioneController().router);
 
-    final pathSegments = request.url.pathSegments;
-    print(pathSegments);
-
-    if (pathSegments.isNotEmpty) {
-      final controllerName = pathSegments.first;
-      switch (controllerName) {
-        /*      case 'autenticazione':
-          return; // userController.handleRequest(request);
-        case 'candidaturaLavoro':
-          return; // productController.handleRequest(request);
-        case 'gestioneEvento':
-          return; // userController.handleRequest(request);
-        case 'gestioneLavoro':
-          return; // productController.handleRequest(request);
-    */
-        case 'gestioneReintegrazione':
-          return ReintegrazioneController().handleRequest(request);
-/*        case 'lavoroAdatto':
-          return; // productController.handleRequest(request);
-        case 'registrazione':
-          return; // userController.handleRequest(request);
-*/
-        default:
-          return Response.notFound('Controller non trovato');
-      }
-    } else {
-      return Response.notFound('Nessun controller specificato');
-    }
+  // Aggiungi la route di fallback per le richieste non corrispondenti
+  app.all('/<ignored|.*>', (Request request) {
+    return Response.notFound('Endpoint non trovato',
+        headers: {'Content-Type': 'text/plain'});
   });
+
+  app.post('/addCorso', (Request request) async {
+    var bodyBytes = await request.read().toList();
+
+    // Percorso in cui desideri salvare l'immagine sul server
+    var directoryPath = 'images'; // Sostituisci con il percorso reale
+
+    // Assicurati che la cartella di destinazione esista, altrimenti creala
+    var directory = Directory(directoryPath);
+    if (!directory.existsSync()) {
+      directory.createSync(recursive: true);
+    }
+
+    // Crea un nome univoco per il file
+    var fileName = 'immagine_${DateTime.now().millisecondsSinceEpoch}.jpg';
+    var filePath = '$directoryPath/$fileName'; // Percorso completo del file
+
+    // Scrivi i byte dell'immagine su disco
+    var file = File(filePath);
+    await file.writeAsBytes(bodyBytes.expand((element) => element).toList());
+
+    // Ora l'immagine è stata salvata su disco
+    return Response.ok('Immagine salvata su disco con successo: $filePath',
+        headers: {'Content-Type': 'text/plain'});
+  });
+
+
+
+
+  // Aggiungi middleware per registrare le richieste
+  final handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(app);
 
   shelf_io.serve(handler, '127.0.0.1', 8080).then((server) {
     print('Server listening on http://${server.address.host}:${server.port}');
