586d487ccd6a6163fe77b7abfbc932549050f526
Mall1o <t.mansi@studenti.unisa.it>
2024-01-08 01:02:13 +0100

    aggiunta gestione token di autenticazione

--- ReStart/lib/model/entity/corso_di_formazione_DTO.dart
+++ ReStart/lib/model/entity/corso_di_formazione_DTO.dart
@@ -1,27 +1,22 @@
 /// Questa classe rappresenta l'entity di un corso di formazione
 class CorsoDiFormazioneDTO {
   ///id univoco di un corso di formazione
-  int? _id;
-
+  late int? _id;
   ///nome del corso di formazione
   late String _nomeCorso;
-
   ///anagrafica del responsabile del corso
   late String _nomeResponsabile;
   late String _cognomeResponsabile;
-
   ///descrizione del croso di formazione
   late String _descrizione;
-
   ///url del sito web dove è possibile seguire il corso di formazione
   late String _urlCorso;
-
   ///immagine associata a un croso di formazione
   late String _immagine;
 
   ///costruttore che permette di istanziare un nuovo corso di formazione nel sistema
   CorsoDiFormazioneDTO({
-    int? id,
+    required int? id,
     required String nomeCorso,
     required String nomeResponsabile,
     required String cognomeResponsabile,
@@ -43,7 +38,7 @@ class CorsoDiFormazioneDTO {
     _immagine = value;
   }
 
-  int? get id => _id;
+  int get id => _id ?? -1; // -1 valore di default che non può essere un id
 
   String get urlCorso => _urlCorso;
 
@@ -75,12 +70,12 @@ class CorsoDiFormazioneDTO {
     _nomeCorso = value;
   }
 
-  /// Factory method per la creazione del DTO da una mappa (solitamente utilizzato quando si deserializzano dati da JSON).
+/// Factory method per la creazione del DTO da una mappa (solitamente utilizzato quando si deserializzano dati da JSON).
   factory CorsoDiFormazioneDTO.fromJson(dynamic json) {
     return CorsoDiFormazioneDTO(
       id: json['id'],
       nomeCorso:
-      json['nome_corso'].toString().replaceAll('[', '').replaceAll(']', ''),
+          json['nome_corso'].toString().replaceAll('[', '').replaceAll(']', ''),
       nomeResponsabile: json['nome_responsabile']
           .toString()
           .replaceAll('[', '')
@@ -94,9 +89,9 @@ class CorsoDiFormazioneDTO {
           .replaceAll('[', '')
           .replaceAll(']', ''),
       urlCorso:
-      json['url_corso'].toString().replaceAll('[', '').replaceAll(']', ''),
+          json['url_corso'].toString().replaceAll('[', '').replaceAll(']', ''),
       immagine:
-      json['immagine'].toString().replaceAll('[', '').replaceAll(']', ''),
+          json['immagine'].toString().replaceAll('[', '').replaceAll(']', ''),
     );
   }
 
@@ -104,17 +99,6 @@ class CorsoDiFormazioneDTO {
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'nome_corso': nomeCorso,
-      'nome_responsabile': nomeResponsabile,
-      'cognome_responsabile': cognomeResponsabile,
-      'descrizione': descrizione,
-      'url_corso': urlCorso,
-      'immagine': immagine,
-    };
-  }
-
-  Map<String, dynamic> toJsonNoID() {
-    return {
       'nome_corso': nomeCorso,
       'nome_responsabile': nomeResponsabile,
       'cognome_responsabile': cognomeResponsabile,
