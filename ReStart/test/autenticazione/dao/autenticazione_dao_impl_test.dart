ac167c4bf03ad953548edd4a53cd284da5ef419c
matpan23 <matteo23baronissi@gmail.com>
2024-01-11 01:58:49 +0100

    update autenticazione test

--- ReStart/test/autenticazione/dao/autenticazione_dao_impl_test.dart
+++ ReStart/test/autenticazione/dao/autenticazione_dao_impl_test.dart
@@ -1,8 +1,8 @@
-// autenticazione_dao_impl_test.dart
+import 'package:restart_all_in_one/model/entity/ads_DTO.dart';
+import 'package:restart_all_in_one/model/entity/ca_DTO.dart';
+import 'package:restart_all_in_one/model/entity/utente_DTO.dart';
 import 'package:test/test.dart';
-import 'package:mockito/mockito.dart';
 import '../../../lib/model/dao/autenticazione/autenticazione_DAO_impl.dart';
-import '../../../lib/model/entity/utente_DTO.dart';
 
 void main() {
   late AutenticazioneDAOImpl dao;
@@ -11,18 +11,157 @@ void main() {
     dao = AutenticazioneDAOImpl();
   });
 
-    group('Test AutenticazioneDAOImpl', () {
-
-      test('Trova utente per username', () async {
-        String utenteMock = 'mariorossi';
-        var result = await dao.findByUsername(utenteMock);
-        expect(result, isNotNull);
-        expect(result.username, equals(utenteMock));
-      });
-
-      test('Utente non trovato', () async {
-        var result = await dao.findByUsername('notfound');
-        expect(result, isNull);
-      });
+  group('Test AutenticazioneDAOImpl', () {
+    test('Trova utente per username', () async {
+      String utenteMock = 'mariorossi';
+      var result = await dao.findByUsername(utenteMock);
+      expect(result, isNotNull);
+      expect(result, equals(utenteMock));
     });
+
+    test('Trova ca per username', () async {
+      String caMock = 'aziendax';
+      var result = await dao.findByUsername(caMock);
+      expect(result, isNotNull);
+      expect(result, equals(caMock));
+    });
+
+    test('Trova ads per username', () async {
+      String adsMock = 'ads_user1';
+      var result = await dao.findByUsername(adsMock);
+      expect(result, isNotNull);
+      expect(result, equals(adsMock));
+    });
+
+    test('Utente non trovato', () async {
+      var result = await dao.findByUsername('notfound');
+      expect(result, isNull);
+    });
+  });
+
+  group('Test removeByUsername', () {
+    test('Eliminazione utente da parte dell ads', () async {
+      String username = 'mariorossi';
+      var result = await dao.removeByUsername(username);
+      expect(result, true);
+    });
+
+    test('Utente non trovato', () async {
+      var result = await dao.removeByUsername('notfound');
+      expect(result, false);
+    });
+  });
+
+  group('Test add', () {
+
+    test('Utente non riconosciuto', () async {
+      var result = await dao.getTipoByUsername('username');
+      expect(result, false);
+    });
+
+    test('Test per utente', () async {
+      UtenteDTO u = UtenteDTO(
+          nome: 'matteo',
+          cognome: 'sileo',
+          cod_fiscale: 'SLEMTT02R23H703F',
+          data_nascita: DateTime(2002, 10, 23),
+          luogo_nascita: 'Salerno',
+          genere: 'M',
+          username: 'PoH',
+          password: 'Pass3',
+          email: 'sileo@gmail.com',
+          num_telefono: '3581798610',
+          immagine: '',
+          via: 'corso garibaldi 53',
+          citta: 'Baronissi',
+          provincia: 'SA');
+      var ug = await dao.getTipoByUsername(u.username);
+      var result = await dao.add(ug);
+      expect(result, true);
+    });
+
+    test('Test per ads', () async {
+      AdsDTO ads = AdsDTO(
+          id: 10,
+          username: 'ads_matteo',
+          password: 'ads_password',
+          email: 'lol@gmail.com',
+          num_telefono: '3264879512',
+          via: 'via lisca 5',
+          citta: 'Baronissi',
+          provincia: 'SA');
+      var ug = await dao.getTipoByUsername(ads.username);
+      var result = await dao.add(ug);
+      expect(result, true);
+    });
+
+    test('Test per ca', () async {
+      CaDTO ca = CaDTO(
+          nome: 'matteo',
+          username: 'ca_matteo',
+          password: 'ca_password',
+          email: 'ca@gmail.com',
+          num_telefono: '3584216970',
+          sito_web: 'www.matteo.com',
+          via: 'gran bosco 23',
+          citta: 'palermo',
+          provincia: 'PA');
+      var ug = await dao.getTipoByUsername(ca.username);
+      var result = await dao.add(ug);
+      expect(result, true);
+    });
+  });
+
+  group('Test update', () {
+    test('Update per utente', () async {
+      UtenteDTO u = UtenteDTO(
+          nome: 'matteo',
+          cognome: 'panza',
+          cod_fiscale: 'PNZMTT02R23H703F',
+          data_nascita: DateTime(2002, 10, 23),
+          luogo_nascita: 'Salerno',
+          genere: 'M',
+          username: 'PoH',
+          password: 'Pass3',
+          email: 'panza@gmail.com',
+          num_telefono: '3581798610',
+          immagine: '',
+          via: 'corso garibaldi 53',
+          citta: 'Baronissi',
+          provincia: 'SA');
+      var ug = await dao.getTipoByUsername(u.username);
+      var result = await dao.update(ug);
+    });
+
+    test('update per ads', () async {
+      AdsDTO ads = AdsDTO(
+          id: 10,
+          username: 'ads_matteo',
+          password: 'ads_password',
+          email: 'panza2@gmail.com',
+          num_telefono: '3264879512',
+          via: 'via lisca 5',
+          citta: 'Baronissi',
+          provincia: 'SA');
+      var ug = await dao.getTipoByUsername(ads.username);
+      var result = await dao.update(ug);
+      expect(result, true);
+    });
+
+    test('update per ca', () async {
+      CaDTO ca = CaDTO(
+          nome: 'matteo',
+          username: 'ca_matteo',
+          password: 'ca_password',
+          email: 'panza3@gmail.com',
+          num_telefono: '3584216970',
+          sito_web: 'www.matteo.com',
+          via: 'gran bosco 23',
+          citta: 'palermo',
+          provincia: 'PA');
+      var ug = await dao.getTipoByUsername(ca.username);
+      var result = await dao.update(ug);
+      expect(result, true);
+    });
+  });
 }
--- ReStart/test/autenticazione/service/autenticazione_service_impl_test.dart
+++ ReStart/test/autenticazione/service/autenticazione_service_impl_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:restart_all_in_one/model/entity/ads_DTO.dart';
 import 'package:restart_all_in_one/model/entity/ca_DTO.dart';
 import 'package:test/test.dart';
@@ -10,18 +12,18 @@ class MockAutenticazioneDAO extends Mock implements AutenticazioneDAO {}
 
 void main() {
   late MockAutenticazioneDAO dao;
-  late AutenticazioneServiceImpl autenticazione;
+  late AutenticazioneServiceImpl service;
 
   setUp(() {
     dao = MockAutenticazioneDAO();
-    autenticazione = AutenticazioneServiceImpl();
+    service = AutenticazioneServiceImpl();
   });
 
   group('Login test', () {
     test('Utente non trovato', () async {
       when(dao.findByUsername('testUser')).thenAnswer((_) async => null);
 
-      final result = await autenticazione.login('testUser', '123');
+      final result = await service.login('testUser', '123');
 
       expect(result, isNull);
     });
@@ -46,7 +48,7 @@ void main() {
       when(dao.findByUsername(mockUtente.username))
           .thenAnswer((_) async => mockUtente);
       var result =
-          await autenticazione.login(mockUtente.username, mockUtente.password);
+          await service.login(mockUtente.username, mockUtente.password);
       expect(result, isNotNull);
       expect(result.token, isNotNull);
     });
@@ -64,7 +66,7 @@ void main() {
       when(dao.findByUsername(mockAds.username))
           .thenAnswer((_) async => mockAds);
       final result =
-          await autenticazione.login(mockAds.username, mockAds.password);
+          await service.login(mockAds.username, mockAds.password);
       expect(result, isA<AdsDTO>());
     });
 
@@ -82,8 +84,73 @@ void main() {
       when(dao.findByUsername(mockca.username))
           .thenAnswer((_) async => mockca);
       final result =
-          await autenticazione.login(mockca.username, mockca.password);
+          await service.login(mockca.username, mockca.password);
       expect(result, isA<AdsDTO>());
+    });
+  });
+
+  group('Test deleteUtente', () {
+    test('Rimozione utente ', () async {
+      UtenteDTO mockUtente = UtenteDTO(
+        username: 'mariorossi',
+        password: 'password1',
+        nome: '',
+        cognome: '',
+        cod_fiscale: '',
+        data_nascita: DateTime.now(),
+        luogo_nascita: '',
+        genere: '',
+        email: '',
+        num_telefono: '',
+        immagine: '',
+        via: '',
+        citta: '',
+        provincia: '',
+      );
+      when(dao.removeByUsername(mockUtente.username))
+          .thenAnswer((_) async => true);
+      var result =
+          await service.deleteUtente(mockUtente.username);
+      expect(result, isNotNull);
+      expect(result, true);
+    });
+  });
+
+  group('Test listaUtenti', () {
+    test('Lista utenti', () async{
+      Future<List<UtenteDTO>> l1 = service.listaUtenti();
+      expect(l1, isNotNull);
+      expect(l1, isA<Future<List<UtenteDTO>>>);
+    });
+  });
+
+  group('Test modifyUtente', () {
+    test('Modify utente', () async{
+      UtenteDTO u = UtenteDTO(
+          nome: 'matteo',
+          cognome: 'panza',
+          cod_fiscale: 'PNZMTT02R23H703F',
+          data_nascita: DateTime(2002, 10, 23),
+          luogo_nascita: 'Salerno',
+          genere: 'M',
+          username: 'PoH',
+          password: 'Pass3',
+          email: 'panza@gmail.com',
+          num_telefono: '3581798610',
+          immagine: '',
+          via: 'corso garibaldi 53',
+          citta: 'Baronissi',
+          provincia: 'SA');
+      var result = await service.modifyUtente(u);
+      expect(result, true);
+    });
+  });
+
+  group('Test visualizzaUtente', () {
+    test('visualizza utente', () {
+      String username = 'mariorossi';
+      var result = service.visualizzaUtente(username);
+      expect(result, isA<Future<UtenteDTO?>>);
     });
   });
 }
