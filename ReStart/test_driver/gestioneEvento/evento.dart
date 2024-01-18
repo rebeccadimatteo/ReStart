c2561e78373f7c5c0cab8adbf92fc845f78cd39f
Mall1o <t.mansi@studenti.unisa.it>
2024-01-18 19:20:36 +0100

    fix varie

--- ReStart/test_driver/gestioneEvento/evento.dart
+++ ReStart/test_driver/gestioneEvento/evento.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:restart_all_in_one/main.dart' as app;
+import 'package:restart/presentation/screens/login_signup/start.dart' as app;
 
 
 void main() {
   enableFlutterDriverExtension();
-
   // Avvia l'app principale
   app.main();
 }
